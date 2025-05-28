9b66234f9b2fd14d6c8f5c85a11c8031c462e4f9
Bharatesh Poojary <bharateshpoojari@gmail.com>
2025-05-28 21:22:47 +0530

    middleware refactored

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 export default clerkMiddleware(async (auth, req: NextRequest) => {
   const { userId } = await auth();
-  if (isPublicRoute) {
-    return NextResponse.next();
-  }
-  if (!userId) {
-    const redirectUrl = new URL("/sign-in", req.url);
-    return NextResponse.redirect(redirectUrl);
-  } else if (userId && req.nextUrl.pathname.startsWith("/c")) {
-    return NextResponse.next();
-  } else {
+  if (userId && isPublicRoute(req)) {
     const chatId = Date.now();
     console.log("chatId", chatId);
     const newUrl = new URL(`/c/${chatId}`, req.url);
     return NextResponse.redirect(newUrl);
   }
+  if (!userId && req.nextUrl.pathname.startsWith("/c(.*)")) {
+    const redirectUrl = new URL("/sign-in", req.url);
+    return NextResponse.redirect(redirectUrl);
+  }
+  return NextResponse.next(); //passing the control to the next middleware
 });
 
 export const config = {
   matcher: [
     "/",
     "/c(.*)",
+    "/sign-in",
+    "/sign-Up",
     "/((?!api|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
 };
