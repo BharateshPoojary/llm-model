418c29350ac91409be1f287bce322a131ae77402
Bharatesh Poojary <bharateshpoojari@gmail.com>
2025-05-28 08:12:10 +0530

    middleware refactored

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 export default clerkMiddleware(async (auth, req: NextRequest) => {
   const { userId } = await auth();
-  if (!userId && !isPublicRoute(req)) {
+  if (!userId) {
     const redirectUrl = new URL("/sign-in", req.url);
     return NextResponse.redirect(redirectUrl);
-  }
-
-  if (userId && req.nextUrl.pathname === "/") {
+  } else {
     const chatId = Date.now();
     console.log("chatId", chatId);
     const newUrl = new URL(`/c/${chatId}`, req.url);
@@ -23,8 +21,8 @@ export const config = {
   matcher: [
     "/",
     "/c(.*)",
-    "/sign-in(.*)",
-    "/sign-up(.*)",
+    "/sign-in",
+    "/sign-up",
     "/((?!api|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
 };
