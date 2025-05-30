d2fd95e8cd0c7423ec7a256f910ce5e7b8b862e5
Bharatesh Poojary <bharateshpoojari@gmail.com>
2025-05-30 08:23:48 +0530

    rectified build error

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -7,7 +7,6 @@ import { Mail, Lock, MailCheck, Loader } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ClerkAPIError } from "@clerk/types";
 import {
   Card,
   CardContent,
